25bc073a93851922ba2c182ee785d075268fe776
IT25101615 <it25101615@my.sliit.lk>
2026-05-05 21:03:44 +0530

    Add files via upload

---
+++ mvnw.cmd
@@ -0,0 +1,76 @@
+@REM ----------------------------------------------------------------------------
+@REM Licensed to the Apache Software Foundation (ASF) under one
+@REM or more contributor license agreements.  See the NOTICE file
+@REM distributed with this work for additional information
+@REM regarding copyright ownership.  The ASF licenses this file
+@REM to you under the Apache License, Version 2.0 (the
+@REM "License"); you may not use this file except in compliance
+@REM with the License.  You may obtain a copy of the License at
+@REM
+@REM    https://www.apache.org/licenses/LICENSE-2.0
+@REM
+@REM Unless required by applicable law or agreed to in writing,
+@REM software distributed under the License is distributed on an
+@REM "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+@REM KIND, either express or implied.  See the License for the
+@REM specific language governing permissions and limitations
+@REM under the License.
+@REM ----------------------------------------------------------------------------
+
+@REM Begin all REM://
+@echo off
+@REM set title of command window
+title %0
+@REM enable extensions
+@setlocal enableextensions
+@setlocal enabledelayedexpansion
+
+set WRAPPER_LAUNCHER=org.apache.maven.wrapper.MavenWrapperMain
+
+@REM Find java.exe
+if defined JAVA_HOME goto findJavaFromJavaHome
+
+set JAVA_EXE=java.exe
+%JAVA_EXE% -version >NUL 2>&1
+if "%ERRORLEVEL%" == "0" goto execute
+echo ERROR: JAVA_HOME is not set and no 'java' command could be found in your PATH.
+goto error
+
+:findJavaFromJavaHome
+set JAVA_HOME=%JAVA_HOME:"=%
+set JAVA_EXE=%JAVA_HOME%/bin/java.exe
+
+if exist "%JAVA_EXE%" goto execute
+echo ERROR: JAVA_HOME is set to an invalid directory: %JAVA_HOME%
+goto error
+
+:execute
+@REM setup the command line
+
+set MAVEN_PROJECTBASEDIR=%~dp0
+
+@REM Find the project base dir
+set WRAPPER_JAR="%MAVEN_PROJECTBASEDIR%\.mvn\wrapper\maven-wrapper.jar"
+set WRAPPER_PROPERTIES="%MAVEN_PROJECTBASEDIR%\.mvn\wrapper\maven-wrapper.properties"
+
+@REM Download maven-wrapper.jar if it doesn't exist
+if exist %WRAPPER_JAR% goto runWrapper
+
+echo Downloading Maven Wrapper...
+@REM Use PowerShell to download
+powershell -Command "& { [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12; Invoke-WebRequest -Uri 'https://repo.maven.apache.org/maven2/org/apache/maven/wrapper/maven-wrapper/3.2.0/maven-wrapper-3.2.0.jar' -OutFile '%MAVEN_PROJECTBASEDIR%\.mvn\wrapper\maven-wrapper.jar' }"
+
+:runWrapper
+"%JAVA_EXE%" ^
+  -classpath %WRAPPER_JAR% ^
+  %WRAPPER_LAUNCHER% %*
+
+if ERRORLEVEL 1 goto error
+goto end
+
+:error
+set ERROR_CODE=1
+
+:end
+@endlocal & set ERROR_CODE=%ERROR_CODE%
+cmd /C exit /B %ERROR_CODE%
